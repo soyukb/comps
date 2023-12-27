a5cd0df5286cb1dba17b731e0f2bdedfee7cb8d7
soyukb <soyukoba@gmail.com>
2023-12-27 13:41:56 +0900

    コメントを入れる

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
   ];
 
   return (
-    <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
   );
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -15,6 +36,7 @@ const Dropdown = ({ options, value, onChange }) => {
   const renderedOptions = options.map((option, index) => {
     return (
       <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
         onClick={() => {
           handleOptionClick(option);
         }}
@@ -26,9 +48,15 @@ const Dropdown = ({ options, value, onChange }) => {
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || "Select..."}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 };
